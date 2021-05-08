8ba5ddb6cf6b9bb05a6fefc174bc8caf13c096b6
Igor Krstic <krsticigor85@gmail.com>
2021-05-08 21:20:52 +0200

    Do Junit

--- CubesTest/src/main/java/com/mycompany/cubestest/Test.java
+++ CubesTest/src/main/java/com/mycompany/cubestest/Test.java
@@ -8,10 +8,21 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class Test {
     
     public static void main (String[] args){
+        // Initiate Webdriver
         WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
         driver.get("http://bvtest.school.cubes.rs/login");
         
-        WebElement bv_username = driver.findElement(By.xpath("//*[@id='app-layout']/div/div/div/div/div[2]/form/div[1]/div/input"));
-        bv_username.sendKeys("Zdravo Jogi");
+        
+        
+        
+        // Find elements
+        WebElement bv_username = driver.findElement(By.xpath("//*[@id=\"app-layout\"]/div/div/div/div/div[2]/form/div[1]/div/input"));
+        WebElement bv_password = driver.findElement(By.xpath("//*[@id=\"app-layout\"]/div/div/div/div/div[2]/form/div[2]/div/input"));
+        WebElement bv_login_button = driver.findElement(By.xpath("//*[@id=\"app-layout\"]/div/div/div/div/div[2]/form/div[4]/div/button"));
+ 
+        bv_username.sendKeys("qa@cubes.rs");
+        bv_password.sendKeys("cubesqa");
+        bv_login_button.click();
     }
 }
